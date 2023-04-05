ce41be39948d89b40e7827cf6d0931ec910df6dd
Ethan De Jesus <ethanawesome702@gmail.com>
2023-04-05 19:15:28 -0400

    ye

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
-list = []
-number = input('number is?')
-number.append(list)
+import random
+
+
+
+def find():
+    list = [1,2,3,4,5,6,7,8,9,10]
+    guess = random.choice(list)
+    user_guess = input('Guess a number between 1 and 10')
+#    while user_guess == guess:
+#        print('Correct')
+        
+    while user_guess == guess:
+        print('correct')
+    if user_guess != guess:
+        for x in range(5):
+            user_guess = input('Try again')
+    print(guess)
+
+    
+   
+
+
+find()
