1e347eb1d7c6752e510c95d347f6b5f16ae8f551
Ethan De Jesus <ethanawesome702@gmail.com>
2023-04-05 20:42:56 -0400

    i did alot of class stuff

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import random
 
 
 
-def find():
+""" def find():
     list = [1,2,3,4,5,6,7,8,9,10]
     guess = random.choice(list)
     user_guess = input('Guess a number between 1 and 10')
@@ -12,12 +12,48 @@ def find():
     while user_guess == guess:
         print('correct')
     if user_guess != guess:
-        for x in range(5):
+        for x in range(15):
             user_guess = input('Try again')
-    print(guess)
+    print(guess) """
 
     
    
 
 
-find()
+
+
+
+class Employee:
+    raise_amount = 2
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+    
+    def fullname(self):
+        return self.first + '.' + self.last
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+class Developer(Employee):
+    def __init__(self,first,last,pay,prog_lang):
+        super().__init__(first,last,pay)
+        self.prog_lang = prog_lang
+class Manager(Employee):
+    def __init__(self,first,last,pay,employees=None):
+        super().__init__(first,last,pay)
+        self.employees = []
+    def add_emp(self,emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+    def add_emp(self,emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+    def print_emps(self):
+        for emp in self.employees:
+            print(emp.fullname())
+
+Ethan = Employee('Ethan', 'DeJesus',50000)
+Future = Developer('Ethan', 'De Jesus', 50000,'Python')
+the_manager = Manager('Boss','Ok',100000,[])
+the_manager.add_emp(Ethan)
+the_manager.print_emps()
