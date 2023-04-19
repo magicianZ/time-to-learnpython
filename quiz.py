447de2303f663cef3e2bc0a2a8db11edece90058
Ethan De Jesus <ethanawesome702@gmail.com>
2023-04-18 20:20:24 -0400

    hmph
    need to make thingy a dictionary

--- quiz.py
+++ quiz.py
@@ -1,4 +1,24 @@
+
+import random
 from PIL import Image
 
-Img=Image.open('D:\\488.PNG')
-Img.show()
+list_of_pictures = {"448.png","squirtle.png"}
+random_guess = random.choice(list_of_pictures)
+def picture():
+    random_guess = random.choice(list_of_pictures)
+    list_of_pictures = {"448.png","squirtle.png"}
+    Img=Image.open(random_guess)
+    Img.show()
+  
+
+
+picture()
+guess = input('Whos that pokemon')
+if guess == random_guess:
+    print('Correct')
+else:
+    print('Incorrect')
+
+
+
+
