e0b3a52ac516e38d478c5578db198fb0673b96a1
Ethan De Jesus <ethanawesome702@gmail.com>
2023-04-19 08:46:26 -0400

    wfe

--- quiz.py
+++ quiz.py
@@ -2,22 +2,17 @@
 import random
 from PIL import Image
 
-list_of_pictures = {"448.png","squirtle.png"}
-random_guess = random.choice(list_of_pictures)
+
 def picture():
+   
+    list_of_pictures = ["448.png", "squirtle.png"]
     random_guess = random.choice(list_of_pictures)
-    list_of_pictures = {"448.png","squirtle.png"}
     Img=Image.open(random_guess)
     Img.show()
-  
 
 
-picture()
-guess = input('Whos that pokemon')
-if guess == random_guess:
-    print('Correct')
-else:
-    print('Incorrect')
+
+
 
 
 
---
+++ test.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import random
+
+
+testing = {"whatever":"Lucario"}
+
+
+
+#Img=Image.open(testing)
+#Img.show()
+print(testing("whatever"))
