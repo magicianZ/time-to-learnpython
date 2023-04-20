ba1bfae6026fe74a610d8da30fc38bbc55854fd3
Ethan De Jesus <ethanawesome702@gmail.com>
2023-04-20 11:38:34 -0400

    parry system go crazy

--- gaming.py
+++ gaming.py
@@ -3,11 +3,12 @@ import random
 
 
 class Player:
-    def __init__(self,HP,Weapon,Name,Damage):
+    def __init__(self,HP,Weapon,Name,Damage,remaining_HP):
         self.HP = HP
         self.Weapon = Weapon
         self.Name = Name
         self.Damage = Damage
+        self.remaining_HP = remaining_HP
         
     def parry(self):
         parry1 = [1,2]
@@ -15,26 +16,30 @@ class Player:
         if chance == 2:
             print('You parried')
         else:
-            pass
+            print('You missed the parry')
+            self.damage()
+
+
   
         
         return chance
     def damage(self):
         chance = self.parry()
         if chance != 2:
-            self.Damage = self.HP - self.Damage
+            self.remaining_HP = self.HP - self.Damage
         
         
 
             
   
 
-Ly = Player(100,"Sword","Lie Lee",1)
+Ly = Player(100,"Sword","Lie Lee",50,100)
+NPC = Player(50,"Claw","Monster",25,50)
 
 Ly.parry()
-Ly.damage()
-print(Ly.HP)
-    
+print(Ly.remaining_HP)
+
+
 
     
 
