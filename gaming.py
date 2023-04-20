f6e4209a67e0af3a1cdeca1f9c7d9234f023521b
Ethan De Jesus <ethanawesome702@gmail.com>
2023-04-20 10:59:04 -0400

    yessir

--- gaming.py
+++ gaming.py
@@ -3,10 +3,11 @@ import random
 
 
 class Player:
-    def __init__(self,HP,Weapon,Name):
+    def __init__(self,HP,Weapon,Name,Damage):
         self.HP = HP
         self.Weapon = Weapon
         self.Name = Name
+        self.Damage = Damage
         
     def parry(self):
         parry1 = [1,2]
@@ -14,14 +15,24 @@ class Player:
         if chance == 2:
             print('You parried')
         else:
-            print('You got hit...')
-            self.HP - 50
+            pass
+  
+        
+        return chance
+    def damage(self):
+        chance = self.parry()
+        if chance != 2:
+            self.Damage = self.HP - self.Damage
+        
+        
+
             
   
 
-Ly = Player(100,"Sword","Lie Lee")
+Ly = Player(100,"Sword","Lie Lee",1)
 
 Ly.parry()
+Ly.damage()
 print(Ly.HP)
     
 
