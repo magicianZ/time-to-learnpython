969df7700f82ef4ede24e1a1d510a748c3886248
Ethan De Jesus <ethanawesome702@gmail.com>
2023-12-18 11:27:07 -0500

    w

---
+++ getoutjoqquinmateo.py
@@ -0,0 +1,28 @@
+word1 =  'abc'
+word2 = 'abcde'
+
+
+def ethan():
+    x = []
+    y = []
+    z = ''
+    if len(word1) > len(word2):
+        e = word2.len()
+
+    else:
+        e = len(word1)
+    
+    for i in range(len(word1)):
+        x.append(word1[i])
+    for i in range(len(word2)):
+        y.append(word2[i])
+    for i in range(e):
+        z+(x[i])
+        z+(y[i])
+        x.remove(x[i])
+        y.remove(y[i])
+    print(z)
+ethan()
+    
+
+
